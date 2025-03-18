ba4ea1586388d3394e224c3ff1deae231731f892
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-03-18 07:54:03 +0100

    feat: add wait() in parent process

--- fork_demo.c
+++ fork_demo.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <wait.h>
 
 void main() {
 
     int child_p = fork();
+    int status = 0;
 
     if (child_p == 0) {
         printf("I am the child process, thus my PID be %d\n", child_p);
-        exit(0);
+        exit(1);
     }
-
+    wait(&status);
     printf("I spawned the child process, which PID was %d\n", child_p);
-
+    printf("The child processed ended, raw status holds %d (WEXITSTATUS: %d)\n", status, WEXITSTATUS(status));	
+    
+       
 }
